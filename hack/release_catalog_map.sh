40866a573ee7c2d96a268fc354966f46fdb3d55d
Jose Valdes <jvaldes@redhat.com>
2025-04-16 16:54:00 -0400

    hack: add release-snapshots.sh script
    This script automates the process of updating
    the `olm.bundle` image in the catalog template,
    rendering the catalog JSON file, and creating
    a new branch with the changes.

---
+++ hack/release_catalog_map.sh
@@ -0,0 +1,24 @@
+declare -A release_catalog_map
+
+# This map is used to determine the catalog version based on the release branch
+release_catalog_map["master"]="v10.19"
+release_catalog_map["release-4.19"]="v10.19"
+release_catalog_map["release-4.18"]="v10.18"
+release_catalog_map["release-4.17"]="v10.17"
+release_catalog_map["release-4.16"]="v10.16"
+release_catalog_map["release-4.15"]="v10.15"
+release_catalog_map["release-4.14"]="v9"
+release_catalog_map["release-4.13"]="v8"
+release_catalog_map["release-4.12"]="v7"
+
+# This function retrieves the catalog version based on the target branch
+# The the catalog version must correspond to the name of the directory containing the catalog-template.json file
+get_catalog() {
+  local target_branch="$1"
+  catalog="${release_catalog_map[$target_branch]}"
+  if [[ -z "${catalog:-}" ]]; then
+    echo "Error: '$target_branch' is not a valid key in release_catalog_map"
+    return 1
+  fi
+  echo "${catalog}"
+}
